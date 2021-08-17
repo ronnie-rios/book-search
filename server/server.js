369b98ae58b32f53ebe3abb70f6782aa8b7cbfde
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-16 20:22:16 -0500

    apolloserver to apollo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//intergrate our apollo server
+async function startUp() {
+  await server.start();
+  server.applyMiddleware({ app }); 
+}
+startUp();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,5 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
