fc61eab8adad9e319d5a46d6a791884b1c0d1ebf
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-22 16:51:39 -0500

    updated searchbook with right mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { SAVE_BOOK } from '../utils/mutations';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
@@ -12,13 +15,15 @@ const SearchBooks = () => {
   const [searchInput, setSearchInput] = useState('');
 
   // create state to hold saved bookId values
-  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-  useEffect(() => {
-    return () => saveBookIds(savedBookIds);
+  const [saveBook, { error }] = useMutation(SAVE_BOOK, {
+    update(cache, { data: { saveBook } }) {
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...saveBook } },
+      });
+    },
   });
+
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
