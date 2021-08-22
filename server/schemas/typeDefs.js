aed3bad927f53a6c0ce87cf649754a1b9459781c
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-22 13:25:28 -0500

    added mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,6 +23,19 @@ const typeDefs = gql`
      type Query {
         me: User
     }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(
+            authors: [String]
+            description: String
+            title: String!
+            bookId: ID!
+            image: String
+            link: String
+        ) : User
+        removeBook(bookId: ID!): User
+    }
 `;
 
 module.exports = typeDefs;
