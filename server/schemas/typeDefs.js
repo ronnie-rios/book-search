eff27e1cee008dffc5a96e6e28008e5b0ece703d
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-22 13:22:42 -0500

    adding typedefs to schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,27 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Query {
-        helloWorld: string;
+   type: {
+       bookId: ID
+       authors: [String]
+       description: String
+       title: String
+       image: String
+       link: String
+   }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedbooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+     type Query {
+        me: User
     }
 `;
 
